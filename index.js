70818ad14304282ab3470097396746c264db925a
Deepali Sharma <106877928+DeepaliShar@users.noreply.github.com>
2022-11-08 14:46:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// anurag
+//Consts
+const apikey = "";
+const apiEndpoint = "";
+
+const apiPaths={
+    fetchAllCategories: '${apiEndpoint}'/genre/movie/list?api_key=${apikey}
+    fetchMoviesList: (id) => '${baseUrl}/dis
+}
+//boots up the app
+function init(){
+    fetchAndBuildAllSections();
+}
+function fetchAndBuildAllSections(){
+    fetch(apiPaths.fetchAllCategories)
+    .then(res => res.json())
+    .then(res => {
+        const categories=res.genres;
+        if(Array.isArray(categories) && categories.length){
+            categories.forEach(category => {
+                fetchAndBuildMovieSection(category);uki
+            });
+        }
+
+        //console.table(movies);
+    })
+    .catch(err=>console.error(err));
+}
+function fetchAndBuildMovieSection(category){
+    console.log(category);
+}
+window.addEventListener('load',function()){
+    init();
+}
